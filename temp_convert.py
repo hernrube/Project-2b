e561b4e04200be20064a86958f06cccdb05a5de6
Ruben Hernandez <rubdog52014@gmail.com>
2024-07-04 14:43:06 -0700

    User inputs a number for Celsius temperature and the output is in Fahrenheit

--- temp_convert.py
+++ temp_convert.py
@@ -1,3 +1,8 @@
+# Author: Ruben Hernandez
+# GitHub username: hernrube
+# Date: 7/04/2024
+# Description: Asks the user to input a number for Celsius temperature
+#              and the output is in Fahrenheit temperature format.
 print("Please enter a Celsius temperature.")
 num=float(input())
 print("The equivalent Fahrenheit temperature is:")
